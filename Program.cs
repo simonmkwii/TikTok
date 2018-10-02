d8d4908dfaf4391dde88aa9c61050f3d5c595586
Simon Aarons <40786398+simonmkwii-dev@users.noreply.github.com>
2018-10-02 12:16:50 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace TikTok
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine($"Usage: TikTok.exe [-setup PRODINFO.bin] ticket.bin");
+            }
+            else
+            {
+                RSAParameters Params;
+                if (!Directory.Exists("RSA") && args[0] == "-setup")
+                {
+                    var ProdFile = File.Open(args[1], FileMode.Open);
+                    var ProdInfo = new BinaryReader(ProdFile);
+
+                    Console.Write("Input the eticket rsa keypair decryption key: ");
+
+                    var Key = HexStrToB(Console.ReadLine());
+
+                    if (!Hex(new SHA256CryptoServiceProvider().ComputeHash(Key)).Equals("46cccf288286e31c931379de9efa288c95c9a15e40b00a4c563a8be244ece515"))
+                    {
+                        throw new ArgumentException("Invalid key!");
+                    }
+
+                    ProdFile.Position += 0x3890;
+
+                    byte[] DecryptedData = AES_CTR(Key, ProdInfo.ReadBytes(0x10), ProdInfo.ReadBytes(0x230));
+
+                    ProdInfo.Close();
+                    ProdFile.Close();
+
+                    BigInteger D = RSAUtils.GetBigInteger(DecryptedData.Take(0x100).ToArray());
+                    BigInteger N = RSAUtils.GetBigInteger(DecryptedData.Skip(0x100).Take(0x100).ToArray());
+                    BigInteger E = RSAUtils.GetBigInteger(DecryptedData.Skip(0x200).Take(0x4).ToArray());
+
+                    Params = RSAUtils.RecoverRSAParameters(N, E, D);
+
+                    Directory.CreateDirectory("RSA");
+
+                    File.WriteAllBytes("RSA/mod.bin", Params.Modulus);
+                    File.WriteAllBytes("RSA/exp.bin", Params.Exponent);
+                    File.WriteAllBytes("RSA/d.bin", Params.D);
+                    File.WriteAllBytes("RSA/p.bin", Params.P);
+                    File.WriteAllBytes("RSA/q.bin", Params.Q);
+                    File.WriteAllBytes("RSA/dp.bin", Params.DP);
+                    File.WriteAllBytes("RSA/dq.bin", Params.DQ);
+                    File.WriteAllBytes("RSA/invq.bin", Params.InverseQ);
+
+                    Console.WriteLine("RSA parameters saved successfully!");
+
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Params = new RSAParameters
+                    {
+                        Modulus = File.ReadAllBytes("RSA/mod.bin"),
+                        Exponent = File.ReadAllBytes("RSA/exp.bin"),
+                        D = File.ReadAllBytes("RSA/d.bin"),
+                        P = File.ReadAllBytes("RSA/p.bin"),
+                        Q = File.ReadAllBytes("RSA/q.bin"),
+                        DP = File.ReadAllBytes("RSA/dp.bin"),
+                        DQ = File.ReadAllBytes("RSA/dq.bin"),
+                        InverseQ = File.ReadAllBytes("RSA/invq.bin")
+                    };
+                }
+
+                var F = File.ReadAllBytes(args[0]);
+
+                string Hex(byte[] In)
+                {
+                    return BitConverter.ToString(In).Replace("-", "").ToLower();
+                }
+
+                var Output = File.OpenWrite("title.keys");
+                var Writer = new StreamWriter(Output);
+
+                var RSA = new RSACng();
+                RSA.ImportParameters(Params);
+
+                for (int i = 0; i < F.Length / 0x400; i++)
+                {
+                    try
+                    {
+                        var Dec = Decrypt_RSA_OAEP_SHA256(F.Skip((i * 0x400) + 0x180).Take(0x100).ToArray(), RSA);
+                        var ID = F.Skip((i * 0x400) + 0x2A0);
+                        Console.WriteLine($"Ticket {i}:");
+                        Console.WriteLine($"    Rights ID:  {Hex(ID.Take(0x10).ToArray())}");
+                        Console.WriteLine($"    Title ID:   {Hex(ID.Take(0x8).ToArray())}");
+                        Console.WriteLine($"    Titlekey:   {Hex(Dec)}");
+                        Writer.WriteLine($"{Hex(ID.Take(0x10).ToArray())} = {Hex(Dec)}");
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                Writer.Close();
+                Output.Close();
+                Console.WriteLine("Done!");
+            }
+        }
+
+        static byte[] AES_CTR(byte[] Key, byte[] CTR, byte[] Data)
+        {
+            var AESCTR = new Aes128CounterMode(CTR);
+            ICryptoTransform Transform;
+            Transform = AESCTR.CreateDecryptor(Key, null);
+            return Transform.TransformFinalBlock(Data, 0, Data.Length);
+        }
+
+        static byte[] Decrypt_RSA_OAEP_SHA256(byte[] Input, RSACng RSA)
+        {
+            var SHA256Padding = RSAEncryptionPadding.CreateOaep(HashAlgorithmName.SHA256);
+            return RSA.Decrypt(Input, SHA256Padding);
+        }
+
+        public static byte[] HexStrToB(string Hex)
+        {
+            return Enumerable.Range(0, Hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(Hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+    }
+}
---
+++ RSAUtils.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace TikTok
+{
+    // Source: https://stackoverflow.com/a/44441955
+    internal class RSAUtils
+    {
+        public static BigInteger GetBigInteger(byte[] Input)
+        {
+            byte[] B = new byte[Input.Length + 1];
+            Buffer.BlockCopy(Input, 0, B, 1, Input.Length);
+            Array.Reverse(B);
+            return new BigInteger(B);
+        }
+
+        public static byte[] GetBytes(BigInteger Input, int Len)
+        {
+            byte[] Bytes = Input.ToByteArray();
+            Len = Bytes.Length;
+            Array.Resize(ref Bytes, Len);
+            Array.Reverse(Bytes);
+            return Bytes;
+        }
+
+        public static BigInteger ModInverse(BigInteger Exp, BigInteger Mod)
+        {
+            BigInteger N = Mod;
+            BigInteger E = Exp;
+            BigInteger T = 0;
+            BigInteger A = 1;
+            while (E != 0)
+            {
+                BigInteger Q = N / E;
+                BigInteger Val;
+                Val = T;
+                T = A;
+                A = Val - Q * A;
+                Val = N;
+                N = E;
+                E = Val - Q * E;
+            }
+            if (T < 0)
+            {
+                T = T + Mod;
+            }
+            return T;
+        }
+
+        public static RSAParameters RecoverRSAParameters(BigInteger n, BigInteger e, BigInteger d)
+        {
+            using (RandomNumberGenerator RNG = RandomNumberGenerator.Create())
+            {
+                BigInteger k = d * e - 1;
+                BigInteger two = 2;
+                BigInteger t = 1;
+                BigInteger r = k / two;
+
+                while (r.IsEven)
+                {
+                    t++;
+                    r /= two;
+                }
+
+                byte[] Buf = n.ToByteArray();
+
+                if (Buf[Buf.Length - 1] == 0)
+                {
+                    Buf = new byte[Buf.Length - 1];
+                }
+
+                BigInteger nMinusOne = n - 1;
+
+                bool Done = false;
+                BigInteger y = BigInteger.Zero;
+
+                for (int i = 0; i < 100 && !Done; i++)
+                {
+                    BigInteger g;
+
+                    do
+                    {
+                        RNG.GetBytes(Buf);
+                        g = GetBigInteger(Buf);
+                    }
+                    while (g >= n);
+
+                    y = BigInteger.ModPow(g, r, n);
+
+                    if (y.IsOne || y == nMinusOne)
+                    {
+                        i--;
+                        continue;
+                    }
+
+                    for (BigInteger j = 1; j < t; j++)
+                    {
+                        BigInteger x = BigInteger.ModPow(y, two, n);
+
+                        if (x.IsOne)
+                        {
+                            Done = true;
+                            break;
+                        }
+
+                        if (x == nMinusOne)
+                        {
+                            break;
+                        }
+
+                        y = x;
+                    }
+                }
+
+                BigInteger p = BigInteger.GreatestCommonDivisor(y - 1, n);
+                BigInteger q = n / p;
+                BigInteger dp = d % (p - 1);
+                BigInteger dq = d % (q - 1);
+                BigInteger inverseQ = ModInverse(q, p);
+
+                int modLen = Buf.Length;
+                int halfModLen = (modLen + 1) / 2;
+
+                return new RSAParameters
+                {
+                    Modulus = GetBytes(n, modLen),
+                    Exponent = GetBytes(e, -1),
+                    D = GetBytes(d, modLen),
+                    P = GetBytes(p, halfModLen),
+                    Q = GetBytes(q, halfModLen),
+                    DP = GetBytes(dp, halfModLen),
+                    DQ = GetBytes(dq, halfModLen),
+                    InverseQ = GetBytes(inverseQ, halfModLen),
+                };
+            }
+        }
+    }
+}
